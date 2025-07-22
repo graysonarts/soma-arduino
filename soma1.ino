ad22c452c87d1936a50edb005dd9c5d56df1bcbb
Grayson <russell.hay@gmail.com>
2025-07-22 06:32:18 -0700

    Button code for testing

--- soma1.ino
+++ soma1.ino
@@ -3,160 +3,75 @@
 uint8_t oldSelectedChannel = 0;
 uint8_t selectedChannel = 0;
 
-#define BTN_CNT 3
+#define ENABLE_SERIAL
+
+#ifdef ENABLE_SERIAL
+#define D(x) Serial.print(x);
+#define DLN(x) Serial.println(x);
+#define DBR Serial.println();
+#else
+#define DBG(x) ;
+#define DBGLN(x) ;
+#define DBGBR ;
+#endif
+
+#define SDA 4
+#define SCL 5
+#define BTN_CNT 12
 #define MCP_ADDR 0x20
 
-int8_t button_pins[BTN_CNT] = {13, 14, 15};
-int8_t button_states[BTN_CNT] = {-1};
+int8_t button_pins[BTN_CNT] = { 16, 17, 18, 19, 20, 21, 22, 26, 27, 0, 1 };
+int8_t button_states[BTN_CNT] = { -1 };
+int8_t led_pins[BTN_CNT] = { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 2, 3 };
+unsigned long last_auto = 0;
 
-MCP23017 mcp = MCP23017(MCP_ADDR, Wire1);
+MCP23017 mcp = MCP23017(MCP_ADDR, Wire);
 
-void setup()
-{
+void setup() {
+  #ifdef ENABLE_SERIAL
+  Serial.begin(115200);
+  while (!Serial);
+  #endif
+  DLN("Start up");
 
-	Serial.begin(115200);
-	delay(1000);
-	Serial.println("Start up");
   // Setup the MCP to be configured the way we need it
-	Wire1.setSDA(2);
-	Wire1.setSCL(3);
-  Wire1.begin();
-	mcp.init();
-	delay(1000);
+  Wire.setSDA(SDA);
+  Wire.setSCL(SCL);
+  Wire.begin();
+  mcp.init();
+  delay(1000);
 
-	Serial.println("Setting MCP ports to output");
-	mcp.portMode(MCP23017Port::A, 0);
-	mcp.portMode(MCP23017Port::B, 0);
+  DLN("Setting MCP ports to output");
+  mcp.portMode(MCP23017Port::A, 0);
+  mcp.portMode(MCP23017Port::B, 0);
 
-	mcp.writeRegister(MCP23017Register::GPIO_A, 0x00);  //Reset port A 
-	mcp.writeRegister(MCP23017Register::GPIO_B, 0x00);  //Reset port B
+  mcp.writeRegister(MCP23017Register::GPIO_A, 0x00);  //Reset port A
+  mcp.writeRegister(MCP23017Register::GPIO_B, 0x00);  //Reset port B
 
-  // Configure button pins for input - We only give a little time
-  for (int i = 0; i < BTN_CNT; i++)
-  {
+  // Configure button pins for input
+  // Configuer led pins for output
+  for (int i = 0; i < BTN_CNT; i++) {
     pinMode(button_pins[i], INPUT_PULLUP);
+    // pinMode(led_pins[i], OUTPUT);
   }
 
-  // Configure led pins for output
 
 
 
   uint8_t conf = mcp.readRegister(MCP23017Register::IODIR_A);
-	Serial.print("IODIR_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-	
-	conf = mcp.readRegister(MCP23017Register::IODIR_B);
-	Serial.print("IODIR_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
+  D("IODIR_A : ");
+  D(conf, BIN);
+  DBR;
 
-	conf = mcp.readRegister(MCP23017Register::IPOL_A);
-	Serial.print("IPOL_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
+  conf = mcp.readRegister(MCP23017Register::IODIR_B);
+  D("IODIR_B : ");
+  D(conf, BIN);
+  DBR;
 
-	conf = mcp.readRegister(MCP23017Register::IPOL_B);
-	Serial.print("IPOL_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::GPINTEN_A);
-	Serial.print("GPINTEN_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::GPINTEN_B);
-	Serial.print("GPINTEN_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::DEFVAL_A);
-	Serial.print("DEFVAL_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::DEFVAL_B);
-	Serial.print("DEFVAL_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::INTCON_A);
-	Serial.print("INTCON_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::INTCON_B);
-	Serial.print("INTCON_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::IOCON);
-	Serial.print("IOCON : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	//conf = mcp.readRegister(IOCONB);
-	//Serial.print("IOCONB : ");
-	//Serial.print(conf, BIN);
-	//Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::GPPU_A);
-	Serial.print("GPPU_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::GPPU_B);
-	Serial.print("GPPU_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::INTF_A);
-	Serial.print("INTF_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::INTF_B);
-	Serial.print("INTF_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::INTCAP_A);
-	Serial.print("INTCAP_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::INTCAP_B);
-	Serial.print("INTCAP_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::GPIO_A);
-	Serial.print("GPIO_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::GPIO_B);
-	Serial.print("GPIO_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::OLAT_A);
-	Serial.print("OLAT_A : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	conf = mcp.readRegister(MCP23017Register::OLAT_B);
-	Serial.print("OLAT_B : ");
-	Serial.print(conf, BIN);
-	Serial.println();
-
-	Serial.println("Startup Done");
-
+  DLN("Startup Done");
 }
 
-void loop()
-{
+void loop() {
   // - Poll the state of all the button pins
   // - Cascade button detection, and only change the state of the selected channel if the button is different than the
   // last time around
@@ -164,36 +79,36 @@ void loop()
   // If we have a new state, update the MCP and turn on the LED for the selected channel, turn off all other leds
   // Wait for 100ms
 
-  for (int i = 0; i < BTN_CNT; i++)
-  {
+  for (int i = 0; i < BTN_CNT; i++) {
     button_states[i] = digitalRead(button_pins[i]);
   }
 
-  for (int i = 0; i < BTN_CNT; i++)
-  {
-    if (button_states[i] == LOW)
-    {
-      selectedChannel = i + 1;
-      break;
+  #ifdef AUTO
+  if (millis() - last_auto > 1000) {
+    Serial.println("Auto Advancing");
+    selectedChannel = (selectedChannel + 1) % BTN_CNT;
+    last_auto = millis();
+  }
+  #endif
+
+  for (int i = 0; i < BTN_CNT; i++) {
+    D(button_states[i] ? "X" : ".");
+    // Set LED channels
+    if (button_states[i] == LOW) {
+      selectedChannel = i;
+      // break;
     }
   }
+  DBR;
 
-  if (selectedChannel != oldSelectedChannel)
-  {
-    Serial.print("switching channel to ");
-    Serial.println(selectedChannel);
-		mcp.writePort(MCP23017Port::A, selectedChannel); 
+
+  if (selectedChannel != oldSelectedChannel) {
+    D("switching channel to ");
+    DLN(selectedChannel | 0x10, BIN);
+    mcp.writePort(MCP23017Port::B, (selectedChannel) | 0x10);
     oldSelectedChannel = selectedChannel;
-    delay(1000);
+    delay(2000);
   }
-  // else
-  // {
-  //   for (int i = 0; i < BTN_CNT; i++)
-  //   {
-  //     Serial.print(button_states[i]);
-  //     Serial.print(" ");
-  //   }
-  //   Serial.println();
-  // }
+
   delay(100);
 }
