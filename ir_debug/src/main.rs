f9843746bd089497703de8d2316ab8ae89b898fa
Grayson <russell.hay@gmail.com>
2025-08-10 21:08:54 -0700

    Add bank and channel test tool

---
+++ ir_debug/src/main.rs
@@ -0,0 +1,13 @@
+fn chan(x: u8) -> u8 {
+    (x & 0x0F) % 4
+}
+
+fn bank(x: u8) -> u8 {
+    (x & 0x0F) / 4
+}
+
+fn main() {
+    for i in 0..=15 {
+        println!("#{:02}: bank({}) chan({})", i, bank(i), chan(i));
+    }
+}
